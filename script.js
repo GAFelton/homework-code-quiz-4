d73deccb745c74c6140071e7ddbb263c6b998545
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-01 14:42:38 -0700

    started writing renderQuestion() function.

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ var introBox = document.getElementsByClassName("intro-box");
 var startButton = document.getElementById("startButton");
 var questionBox = document.getElementsByClassName("question-box");
 var questionHeader = document.getElementById("questionHeader");
-var qButton1 = document.getElementById("questionButton1");
-var qButton2 = document.getElementById("questionButton2");
-var qButton3 = document.getElementById("questionButton3");
-var qButton4 = document.getElementById("questionButton4");
+var questionArea = document.getElementById("questionArea");
+// var qButton1 = document.getElementById("questionButton1");
+// var qButton2 = document.getElementById("questionButton2");
+// var qButton3 = document.getElementById("questionButton3");
+// var qButton4 = document.getElementById("questionButton4");
 var secondsLeft = 75;
+var questions = {
+    "q1": ["question1", "answer1", "answer2", "answer3", "answer4"],
+    "q2": ["question2", "answer1", "answer2", "answer3", "answer4"],
+    "q3": ["question3", "answer1", "answer2", "answer3", "answer4"],
+    "q4": ["question4", "answer1", "answer2", "answer3", "answer4"],
+    "q5": ["question5", "answer1", "answer2", "answer3", "answer4"]
+};
 
 var highScore = localStorage.getItem("high-score");
 
@@ -36,6 +44,19 @@ function setTime() {
     }, 1000);
   }
 
+function renderQuestion() {
+    questionArea.innerHTML = "";
+    questionHeader.textContent = questions.q1[0];
+
+    for (var j = 1; j < questions.q1.length; j++) {
+        var question1 = questions.q1[j];
+
+        var button = document.createElement("button");
+        button.textcontent = question1;
+        button.setAttribute("data-index", j);
+        questionArea.appendChild(button);
+    }
+}
 
 
 /* CODE QUIZ PSEUDOCODE
